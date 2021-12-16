b65012c73ee23a16b4f27c77f02e82c0602c49c7
Mel1Am <mel1am@hotmail.com>
2021-12-16 02:34:37 -0600

    Update

--- Programa1/src/App.java
+++ Programa1/src/App.java
@@ -1,5 +1,7 @@
+import java.util.logging.Logger;
+
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args){
         System.out.println("Hello, World!");
     }
 }
